62f69ee8410c6ca7b0d4e5e2aef64d36701ac356
Augumat <augumat@gmail.com>
2022-03-15 01:33:54 -0500

    (big one) converted entirely to winapi resource packaging

---
+++ resloader.h
@@ -0,0 +1,4 @@
+// created to isolate windows api calls from raylib due to conflicts
+
+// function to load packaged resources
+unsigned char* LoadPackagedResource(unsigned long typeId, unsigned long resourceId, unsigned long* size);
--- resource.h
+++ resource.h
@@ -1,2 +1,27 @@
 // Used by glass.rc
-#define IDI_GLASS 10
+
+// define datatypes
+#define PNG_RESOURCE 1380
+#define WAV_RESOURCE 1381
+#define TIERSPEC_RESOURCE 1382
+
+// exe icon (.ico)
+#define IDI_GLASS 0
+
+// tier icons
+#define TIER0_IMG 1
+#define TIER1_IMG 2
+#define TIER2_IMG 3
+#define TIER3_IMG 4
+#define TIER4_IMG 5
+#define TIER5_IMG 6
+#define TIER6_IMG 7
+#define TIER7_IMG 8
+#define TIER8_IMG 9
+#define TIER9_IMG 10
+#define TIER10_IMG 11
+
+// sound effects
+#define BOUNCE_SOUND 12
+#define HIT_SOUND 13
+#define GAME_OVER_SOUND 14
